35c70b77a68b4fd06a236bc90967ca939ceef586
Shivam <shivam0sharma85@gmail.com>
2020-12-09 12:50:11 +0530

    first commit

---
+++ android/app/src/main/java/com/example/edituser/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.edituser;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'personal.dart';
+import 'car.dart';
+void main() => runApp(MaterialApp(
+  home:Homepage()
+));
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage > {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Edit User details'),
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        elevation: 1.0,
+      ),
+    body: Padding(
+      padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget> [
+              SizedBox(height: 50),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder:(context)=> Edit()),
+                  );
+                },
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 20),
+                      Text('Personal',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.purpleAccent,
+                      ),
+                      ),
+                      SizedBox(width: 130),
+                      Icon(
+                        Icons.person,
+                        color: Colors.purpleAccent,
+                        size: 100,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 105),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder:(context)=> Car()),
+                  );
+                },
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 20),
+                      Text('Car',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.purpleAccent,
+                        ),
+                      ),
+                      SizedBox(width: 200),
+                      Icon(
+                        Icons.directions_car,
+                        color: Colors.purpleAccent,
+                        size: 100,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 105),
+              Container(
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                ButtonTheme(
+                  minWidth: 350.0,
+                  height: 70.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text("Cancel",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                    color: Colors.red,
+                  ),
+                ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+    ),
+    );
+  }
+}
+
+
+
+
